8557baa8d20aff6f8165cb257b558242f320a8f3
jackzzs <jackzzs@outlook.com>
2024-05-04 23:38:50 +0800

    Apply renaming

--- setup.py
+++ setup.py
@@ -11,23 +11,23 @@ from textwrap import dedent
 from setuptools import Extension, find_packages, setup
 from setuptools.command.build_ext import build_ext as build_ext_orig
 
-# RDKit version to build (tag from github repository)
-rdkit_tag = "Release_2023_03_1"
+# RDKix version to build (tag from github repository)
+rdkix_tag = "Release_2023_03_1"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 
-class RDKit(Extension):
+class RDKix(Extension):
     def __init__(self, name, **kwargs):
         super().__init__(name, sources=[])
         self.__dict__.update(kwargs)
 
 
-class BuildRDKit(build_ext_orig):
+class BuildRDKix(build_ext_orig):
     def run(self):
         for ext in self.extensions:
-            self.build_rdkit(ext)
+            self.build_rdkix(ext)
         super().run()
 
     def get_ext_filename(self, ext_name):
@@ -105,13 +105,13 @@ class BuildRDKit(build_ext_orig):
 
         check_call(cmd)
 
-    def build_rdkit(self, ext):
-        """Build RDKit
+    def build_rdkix(self, ext):
+        """Build RDKix
 
         Steps:
         (1) Use Conan to install boost and other libraries
-        (2) Build RDKit
-        (3) Copy RDKit and additional files to the wheel path
+        (2) Build RDKix
+        (3) Copy RDKix and additional files to the wheel path
         (4) Copy the libraries to system paths
         """
 
@@ -122,26 +122,26 @@ class BuildRDKit(build_ext_orig):
         conan_toolchain_path.mkdir(parents=True, exist_ok=True)
         self.conan_install(conan_toolchain_path)
 
-        # (2) Build RDkit
+        # (2) Build RDkix
         # Define paths
         build_path = Path(self.build_temp).absolute()
         build_path.mkdir(parents=True, exist_ok=True)
         os.chdir(str(build_path))
 
         # Install path
-        rdkit_install_path = build_path / "rdkit_install"
-        rdkit_install_path.mkdir(parents=True, exist_ok=True)
+        rdkix_install_path = build_path / "rdkix_install"
+        rdkix_install_path.mkdir(parents=True, exist_ok=True)
 
-        # Clone RDKit from git at rdkit_tag
+        # Clone RDKix from git at rdkix_tag
         check_call(
-            ["git", "clone", "-b", f"{ext.rdkit_tag}", "https://github.com/rdkit/rdkit"]
+            ["git", "clone", "-b", f"{ext.rdkix_tag}", "https://github.com/rdkix/rdkix"]
         )
 
         # Location of license file
-        license_file = build_path / "rdkit" / "license.txt"
+        license_file = build_path / "rdkix" / "license.txt"
 
         # Start build process
-        os.chdir(str("rdkit"))
+        os.chdir(str("rdkix"))
 
         # Define CMake options
         options = [
@@ -149,7 +149,7 @@ class BuildRDKit(build_ext_orig):
             # Select correct python interpreter
             f"-DPYTHON_EXECUTABLE={sys.executable}",
             f"-DPYTHON_INCLUDE_DIR={get_paths()['include']}",
-            # RDKit build flags
+            # RDKix build flags
             "-DRDK_BUILD_INCHI_SUPPORT=ON",
             "-DRDK_BUILD_AVALON_SUPPORT=ON",
             "-DRDK_BUILD_PYTHON_WRAPPERS=ON",
@@ -161,7 +161,7 @@ class BuildRDKit(build_ext_orig):
             # Disable system libs for finding boost
             "-DBoost_NO_SYSTEM_PATHS=ON",
             # build stuff
-            f"-DCMAKE_INSTALL_PREFIX={rdkit_install_path}",
+            f"-DCMAKE_INSTALL_PREFIX={rdkix_install_path}",
             "-DCMAKE_BUILD_TYPE=Release",
             # Speed up builds
             "-DRDK_BUILD_CPP_TESTS=OFF",
@@ -171,7 +171,7 @@ class BuildRDKit(build_ext_orig):
 
         # Modifications for Windows
         if sys.platform == "win32":
-            # DRDK_INSTALL_STATIC_LIBS should be fixed in newer RDKit builds
+            # DRDK_INSTALL_STATIC_LIBS should be fixed in newer RDKix builds
             options += [
                 "-Ax64",
                 "-DRDK_INSTALL_STATIC_LIBS=OFF",
@@ -218,7 +218,7 @@ class BuildRDKit(build_ext_orig):
             "cmake --install build",
         ]
 
-        # Run CMake and install RDKit
+        # Run CMake and install RDKix
         [
             check_call(
                 shlex.split(c, posix="win32" not in sys.platform),
@@ -229,12 +229,12 @@ class BuildRDKit(build_ext_orig):
 
         os.chdir(str(cwd))
 
-        # (3) Copy RDKit and additional files to the wheel path
+        # (3) Copy RDKix and additional files to the wheel path
         py_name = "python" + ".".join(map(str, sys.version_info[:2]))
-        rdkit_files = rdkit_install_path / "lib" / py_name / "site-packages" / "rdkit"
+        rdkix_files = rdkix_install_path / "lib" / py_name / "site-packages" / "rdkix"
 
         if sys.platform == "win32":
-            rdkit_files = rdkit_install_path / "Lib" / "site-packages" / "rdkit"
+            rdkix_files = rdkix_install_path / "Lib" / "site-packages" / "rdkix"
 
         # Modify RDPaths.py
         sed = "gsed" if sys.platform == "darwin" else "sed"
@@ -243,32 +243,32 @@ class BuildRDKit(build_ext_orig):
                 sed,
                 "-i",
                 "/_share =/c\_share = os.path.dirname(__file__)",  # noqa: W605
-                f"{rdkit_files / 'RDPaths.py'}",
+                f"{rdkix_files / 'RDPaths.py'}",
             ]
         )
 
         # Data directory
-        rdkit_data_path = rdkit_install_path / "share" / "RDKit" / "Data"
+        rdkix_data_path = rdkix_install_path / "share" / "RDKix" / "Data"
 
         # Contrib directory
-        rdkit_contrib_path = rdkit_install_path / "share" / "RDKit" / "Contrib"
+        rdkix_contrib_path = rdkix_install_path / "share" / "RDKix" / "Contrib"
 
-        # copy rdkit files here, make sure it's empty
+        # copy rdkix files here, make sure it's empty
         wheel_path = Path(self.get_ext_fullpath(ext.name)).absolute()
         if wheel_path.exists():
             rmtree(str(wheel_path))
 
         # Copy the Python files
-        copytree(str(rdkit_files), str(wheel_path))
+        copytree(str(rdkix_files), str(wheel_path))
 
         # Copy the data directory
-        copytree(str(rdkit_data_path), str(wheel_path / "Data"))
+        copytree(str(rdkix_data_path), str(wheel_path / "Data"))
 
         # Copy the contrib directory
-        copytree(str(rdkit_contrib_path), str(wheel_path / "Contrib"))
+        copytree(str(rdkix_contrib_path), str(wheel_path / "Contrib"))
 
         # Delete some large files from the Contrib folder
-        # that are not necessary for running RDKit
+        # that are not necessary for running RDKix
         # See https://github.com/rdkit/rdkit/issues/5601
         _dir = wheel_path / "Contrib" / "NIBRSubstructureFilters"
         rmtree(str(_dir / "examples"))
@@ -284,32 +284,32 @@ class BuildRDKit(build_ext_orig):
         copy_file(str(license_file), str(wheel_path))
 
         # (4) Copy the libraries to system paths
-        rdkit_root = rdkit_install_path / "lib"
+        rdkix_root = rdkix_install_path / "lib"
 
         if "linux" in sys.platform:
             to_path = Path("/usr/local/lib")
-            [copy_file(i, str(to_path)) for i in rdkit_root.glob("*.so*")]
+            [copy_file(i, str(to_path)) for i in rdkix_root.glob("*.so*")]
         elif "win32" in sys.platform:
             # windows is Lib or libs?
             to_path = Path("C://libs")
             to_path.mkdir(parents=True, exist_ok=True)
-            [copy_file(i, str(to_path)) for i in rdkit_root.glob("*.dll")]
+            [copy_file(i, str(to_path)) for i in rdkix_root.glob("*.dll")]
 
         elif "darwin" in sys.platform:
             to_path = Path("/usr/local/lib")
-            [copy_file(i, str(to_path)) for i in rdkit_root.glob("*dylib")]
+            [copy_file(i, str(to_path)) for i in rdkix_root.glob("*dylib")]
 
 
 setup(
-    name="rdkit",
-    version=rdkit_tag.replace("Release_", "").replace("_", "."),
+    name="rdkix",
+    version=rdkix_tag.replace("Release_", "").replace("_", "."),
     description="A collection of chemoinformatics and machine-learning software written in C++ and Python",
     author="Christopher Kuenneth",
     author_email="chris@kuenneth.dev",
-    url="https://github.com/kuelumbus/rdkit-pypi",
+    url="https://github.com/kuelumbus/rdkix",
     project_urls={
-        "RDKit": "http://rdkit.org/",
-        "RDKit on Github": "https://github.com/rdkit/rdkit",
+        "RDKix": "http://rdkit.org/",
+        "RDKix on Github": "https://github.com/rdkix/rdkix",
     },
     license="BSD-3-Clause",
     long_description=long_description,
@@ -320,7 +320,7 @@ setup(
         "Pillow",
     ],
     ext_modules=[
-        RDKit("rdkit", rdkit_tag=rdkit_tag),
+        RDKix("rdkix", rdkix_tag=rdkix_tag),
     ],
-    cmdclass=dict(build_ext=BuildRDKit),
+    cmdclass=dict(build_ext=BuildRDKix),
 )
